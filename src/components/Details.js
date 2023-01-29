e62126d2541601e679e0a3d14d8157b1ac37a3ee
Md Ghulam Zilani <zilaniak786123@gmail.com>
2023-01-29 18:59:11 +0530

    saving commit

--- src/components/Details.js
+++ src/components/Details.js
@@ -48,7 +48,7 @@ const Details = () => {
             </p>
 
         </div>
-        <div>
+        <div className='col-12'>
         <iframe width="560" height="315" src={data?.youtube} title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
         </div>
         </>
